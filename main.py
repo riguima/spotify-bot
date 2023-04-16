608ce70da1d42851b29ffbf18af5407e9e878ccb
riguima <richard.alexsander.guima@gmail.com>
2023-04-16 11:51:35 -0300

    fix: fix main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 
 from spotify_bot.repositories import AccountRepository
-from spotify_bot.use_cases import listen_playlist
+from spotify_bot.use_cases import listen_playlist, make_login
 
 
 if __name__ == '__main__':
@@ -11,6 +11,12 @@ if __name__ == '__main__':
     accounts = AccountRepository().all()
     for _ in range(10):
         for i in range(len(accounts) // drivers_amount):
-            listen_playlist(
-                accounts[i * drivers_amount:i * drivers_amount + drivers_amount],
-                playlist_url)
+            while True:
+                listen_accounts = accounts[
+                    i * drivers_amount:i * drivers_amount + drivers_amount]
+                make_logins = [make_login(a) for a in listen_accounts]
+                if make_logins.count(False) > 0:
+                    del accounts[make_logins.index(False)]
+                else:
+                    break
+            listen_playlist(listen_accounts, playlist_url)
